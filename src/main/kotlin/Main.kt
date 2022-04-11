e95fdc6452407cbb3005a1299c059f03085e3965
MageeMoose <57415911+MageeMoose@users.noreply.github.com>
2022-04-11 23:54:52 +0200

    Added function FindTaxRate and CalculateSalary

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,10 +6,43 @@ val taxTiers = arrayOf(
     TaxTier(band = 120000.0, tier = .55),
 )
 fun main(args: Array<String>) {
+    var salary: Double
+    var netSalary: Salary
+    println("Hi, enter your monthly salary:")
+    salary = readLine()!!.toDouble()
+    netSalary = CalculateSalary(salary, taxTiers)
+    println("Your net salary is: ${netSalary.net}")
+    println("And your tax is ${netSalary.taxRate.rateText}")
+}
+
+fun  CalculateSalary(salary: Double, taxRates: Array<TaxTier> ): Salary{
+    var taxTier = FindTaxRate(salary,taxRates)
+
+    return Salary(gross = salary, taxRate = taxTier)
 
 }
 
+fun  FindTaxRate (grossSalary: Double, taxRate: Array<TaxTier> ): TaxTier{
+    for ( tier in taxRate.sortedByDescending { T -> T.band })
+    {
+        if (grossSalary >= tier.band)
+        {
+            return tier
+        }
+    }
+
+    return TaxTier(band = grossSalary, tier = 0.0)
+}
 data class TaxTier(
     var band: Double,
-    var tier: Double
+    var tier: Double,
+    var rateText: String = "${tier * 100} %",
+
+)
+
+data class Salary (
+    var gross: Double,
+    var taxRate: TaxTier,
+    var taxAmount: Double = gross * taxRate.tier,
+    var net: Double = gross - taxAmount
 )
